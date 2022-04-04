63659238a9d958fbb3ef1a331053702c33eaebb8
baophan1508 <phnbao18@apcs.vn>
2022-04-05 00:01:25 +0700

    first commit

---
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,96 @@
+package com.example.demo.student;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+
+@Entity
+@Table
+public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_squence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
+    private Long Id;
+    private String name;
+    private LocalDate dob;
+    private String email;
+    @Transient
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, LocalDate dob, String email) {
+        Id = id;
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Student(String name, LocalDate dob, String email) {
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
